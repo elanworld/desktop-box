86d64ceda5aaa526cacfa8207ffabc4625d880f0
alan <elanworld@163.com>
2022-08-12 12:24:51 +0800

    增加眼睛

--- desktop-box/Form1.Designer.cs
+++ desktop-box/Form1.Designer.cs
@@ -1,4 +1,5 @@
 ﻿namespace desktop_box
+
 {
     partial class Form1
     {
@@ -11,6 +12,8 @@
         /// 清理所有正在使用的资源。
         /// </summary>
         /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        /// 
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -23,7 +26,7 @@
         #region Windows 窗体设计器生成的代码
 
         /// <summary>
-        /// 设计器支持所需的方法 - 不要修改
+        /// 设计器支持所需的方法 - 不要
         /// 使用代码编辑器修改此方法的内容。
         /// </summary>
         private void InitializeComponent()
@@ -32,16 +35,26 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(433, 163);
+            this.ClientSize = new System.Drawing.Size(356, 290);
+            this.ControlBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
+            this.Opacity = 1D;
             this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
     }
 }
 
--- desktop-box/Form1.cs
+++ desktop-box/Form1.cs
@@ -1,20 +1,146 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace desktop_box
 {
     public partial class Form1 : Form
     {
+        private bool bFormDragging;
+        private Point oPointClicked;
+
         public Form1()
         {
             InitializeComponent();
         }
+        #region 重载
+
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            InitializeStyles();
+            base.OnHandleCreated(e);
+        }
+
+        private void InitializeStyles()
+        {
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.UserPaint, true);
+            UpdateStyles();
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cParms = base.CreateParams;
+                cParms.ExStyle |= 0x00080000; // WS_EX_LAYERED
+                return cParms;
+            }
+        }
+
+        #endregion
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ComponentResourceManager resources = new ComponentResourceManager(typeof(Form1));
+            Bitmap bitmap = (Bitmap) resources.GetObject("eyes");
+
+            SetBits(ResizeBitMap(bitmap, 200));
+        }
+
+               private void Form1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                bFormDragging = true;
+                oPointClicked = new Point(e.X, e.Y);
+                MoveWindows(222, 540);
+            }
+        }
+
+        private void Form1_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                bFormDragging = false;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (bFormDragging)
+            {
+                Point oMoveToPoint = default(Point);
+                //以当前鼠标位置为基础，找出目标位置
+                oMoveToPoint = PointToScreen(new Point(e.X, e.Y));
+                oMoveToPoint.Offset(oPointClicked.X * -1, (oPointClicked.Y + SystemInformation.CaptionHeight + SystemInformation.BorderSize.Height) * -1 + 24);
+                Location = oMoveToPoint;
+            }
+        }
+
+        public void MoveWindows(int width, int height)
+        {
+            Location = new Point(width, height);
+        }
+
+        public void emptBitMap()
+        {
+            SetBits(new Bitmap(1, 1));
+        }
+
+
+        public Bitmap ResizeBitMap(Bitmap bitmap, int width)
+        {
+            return new Bitmap(bitmap, width, width * bitmap.Height / bitmap.Width);
+        }
+
+
+        public void SetBits(Bitmap bitmap)
+        {
+
+            if (!Bitmap.IsCanonicalPixelFormat(bitmap.PixelFormat) || !Bitmap.IsAlphaPixelFormat(bitmap.PixelFormat))
+                MessageBox.Show("Error Bitmap");
+
+            IntPtr oldBits = IntPtr.Zero;
+            IntPtr screenDC = Win32.GetDC(IntPtr.Zero);
+            IntPtr hBitmap = IntPtr.Zero;
+            IntPtr memDc = Win32.CreateCompatibleDC(screenDC);
+
+            try
+            {
+                Win32.Point topLoc = new Win32.Point(Left, Top);
+                Win32.Size bitMapSize = new Win32.Size(bitmap.Width, bitmap.Height);
+                Win32.BLENDFUNCTION blendFunc = new Win32.BLENDFUNCTION();
+                Win32.Point srcLoc = new Win32.Point(0, 0);
+
+                hBitmap = bitmap.GetHbitmap(Color.FromArgb(0));
+                oldBits = Win32.SelectObject(memDc, hBitmap);
+
+                blendFunc.BlendOp = Win32.AC_SRC_OVER;
+                blendFunc.SourceConstantAlpha = 255;
+                blendFunc.AlphaFormat = Win32.AC_SRC_ALPHA;
+                blendFunc.BlendFlags = 0;
+
+                Win32.UpdateLayeredWindow(Handle, screenDC, ref topLoc, ref bitMapSize, memDc, ref srcLoc, 0, ref blendFunc, Win32.ULW_ALPHA);
+            }
+            finally
+            {
+                if (hBitmap != IntPtr.Zero)
+                {
+                    Win32.SelectObject(memDc, oldBits);
+                    Win32.DeleteObject(hBitmap);
+                }
+                Win32.ReleaseDC(IntPtr.Zero, screenDC);
+                Win32.DeleteDC(memDc);
+            }
+        }
+
     }
 }
+   
+
